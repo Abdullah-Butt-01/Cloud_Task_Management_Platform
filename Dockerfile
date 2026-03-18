7fa00d9187b2a980f469080509a3cdd1152797bc
Abdullah Butt <abdullahbuttpkr15@gmail.com>
2026-03-18 13:33:21 +0500

    Phase 3 done

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Use official Python image
+FROM python:3.10-slim
+
+# Set working directory
+WORKDIR /app
+
+# Copy requirements and install
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy app code
+COPY app/ ./app
+
+# Set environment variables
+ENV FLASK_APP=app/app.py
+ENV FLASK_RUN_HOST=0.0.0.0
+ENV FLASK_RUN_PORT=5000
+
+# Expose port
+EXPOSE 5000
+
+# Start Flask
+CMD ["flask", "run"]
--- app/app.py
+++ app/app.py
@@ -1,23 +1,28 @@
 from flask import Flask
 from app import db
-import os
+import os # For reading Environment variables (env)
 
-def create_app():
+import redis
+
+redis_host = os.getenv("REDIS_HOST", "redis")
+r = redis.Redis(host=redis_host, port=6379)
+
+def create_app(): # Instead creatting globally, create inside function to avoid circular imports
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
         "DATABASE_URL",
-        "postgresql://postgres:password@localhost:5432/tasks"
-    )
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        "postgresql://postgres:password@db:5432/tasks"
+    ) # If env exists, use it otherwise use default value
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False # SQLAlchemy tracking (changes in memory)
 
-    db.init_app(app)  # initialize db here
+    db.init_app(app)  # before this, Flask and DB exists but not connected
 
-    # import models AFTER db.init_app
-    from app.models.task import Task
+    # import models AFTER db.init_app (database instance should exist first)
+    from app.models.task import Task 
 
     # import and register blueprints
     from app.routes.tasks import tasks_bp
-    app.register_blueprint(tasks_bp)
+    app.register_blueprint(tasks_bp) # add routes to the application
 
     # create tables (for development)
     with app.app_context():
@@ -25,7 +30,8 @@ def create_app():
 
     @app.route("/")
     def home():
-        return {"message": "Task Platform API running"}
+        count = r.incr("hits")
+        return {"message": f"Task Platform API running - visit: {count}"}
 
     return app
 
