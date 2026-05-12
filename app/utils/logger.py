a2b57821fb619c57fb837661625c211bd48ee930
Abdullah Butt <abdullahbuttpkr15@gmail.com>
2026-05-12 13:13:19 +0500

    Updaying logger

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -6,12 +6,15 @@ def setup_logger():
       format="%(asctime)s | %(levelname)s | %(message)s",
     )
 
-def log_message(component, message, task_id=None, user_id=None):
+def log_message(component, message, task_id=None, user_id=None, file_job_id=None):
 
     base = f"[{component}]"
 
     if task_id:
       base += f" [TASK_ID={task_id}]"
+
+    if file_job_id:
+      base += f" [FILE_JOB_ID={file_job_id}]"
 
     if user_id:
       base += f"USER={user_id}"
